b4159eace0f8133b4ff1cf0f7a01080203f3f214
Erik Firdaus <erikfirdaus03@gmail.com>
2023-07-11 13:51:31 +0700

    menjadikan satu komponen

--- App.js
+++ App.js
@@ -48,107 +48,53 @@ const Stage = () => {
     }
   };
 
-  const Snare2 = () => {
-    return (
-      <TouchableOpacity onPress={() => playsound("snare")}>
-        <View style={styles.Snare2}>
-          <Image source={imageSnare2} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Snare = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.Snare}>
-          <Image source={imageSnare} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Ride = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.ride}>
-          <Image source={imageRide} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Close = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.close}>
-          <Image source={imageclose} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Tom = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.Tom}>
-          <Image source={imageTom} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Splash = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.Splash}>
-          <Image source={imageSplash} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Kick = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.Kick}>
-          <Image source={imageKick} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Open = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.Open}>
-          <Image source={imageOpen} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
-  const Crash = () => {
-    return (
-      <TouchableOpacity>
-        <View style={styles.crash}>
-          <Image source={imageCrash} />
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <Open />
-      <Crash />
-      <Kick />
-      <Splash />
-      <Tom />
-      <Close />
-      <Ride />
-      <Snare />
-      <Snare2 />
+      <View style={styles.crash}>
+        <TouchableOpacity>
+          <Image source={imageCrash} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.open}>
+        <TouchableOpacity>
+          <Image source={imageOpen} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.kick}>
+        <TouchableOpacity>
+          <Image source={imageKick} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.splash}>
+        <TouchableOpacity>
+          <Image source={imageSplash} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tom}>
+        <TouchableOpacity>
+          <Image source={imageTom} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.close}>
+        <TouchableOpacity>
+          <Image source={imageclose} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.ride}>
+        <TouchableOpacity>
+          <Image source={imageRide} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.snare}>
+        <TouchableOpacity>
+          <Image source={imageSnare} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.snare2}>
+        <TouchableOpacity>
+          <Image source={imageSnare2} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -170,17 +116,17 @@ const styles = StyleSheet.create({
     top: 50,
     left: 150,
   },
-  Snare2: {
+  snare2: {
     position: "absolute",
     top: 136,
     left: 268,
   },
-  Kick: {
+  kick: {
     position: "absolute",
     top: 72,
     left: 290,
   },
-  Splash: {
+  splash: {
     position: "absolute",
     top: 230,
     left: 160,
@@ -190,17 +136,17 @@ const styles = StyleSheet.create({
     top: 165,
     left: 474,
   },
-  Open: {
+  open: {
     position: "absolute",
     top: 266,
     left: 481,
   },
-  Tom: {
+  tom: {
     position: "absolute",
     top: 243,
     left: 408,
   },
-  Snare: {
+  snare: {
     position: "absolute",
     top: 243,
     left: 270,
